f00886aa6475e1c0180b6e099f064a80d17d8681
slabnevitch <handofschpilrain@mail.ru>
2022-01-26 14:03:26 +0500

    catalog page added

--- js/common.js
+++ js/common.js
@@ -11,6 +11,71 @@ jQuery(function() {
 		}
 	// End ibg class
 
+	// mobile catalog filter handler
+	if($('.mob-sort').length > 0){
+		(function() {
+			var $filter = $('#filter-bar'),
+					$filterOpener = $('#mob-filters-open'),
+					$filterCloser = $('#mob-filters-close'),
+					$sortToggler = $('#mob-sort-open'),
+					$html = $('html');
+
+			init = function() {
+				console.log('init!')
+				events();
+				mediaQuerry();
+			}
+
+			mediaQuerry = function() {
+				var mediaQuery = window.matchMedia('(max-width: 740px)');//breakpoint for dl-menu activation
+
+		    mediaQuery.addListener(mediaQueryChange);
+		    mediaQueryChange(mediaQuery);
+			}
+	    mediaQueryChange = function(e) {
+	      if (!e.matches) {
+	      	filterClose();
+	      }
+	    }
+
+			events = function() {
+				$filterOpener.on('click', filterOpen);
+				$filterCloser.on('click', filterClose);
+				$sortToggler.on('click', sortToggle);
+				$(document).on('click', docClick);
+			}
+
+			filterOpen = function() {
+				$filter.addClass('active');
+				$html.addClass('freeze');
+			}
+
+			filterClose = function() {
+				$filter.removeClass('active');
+				$html.removeClass('freeze');
+			}
+
+			docClick = function(e) {
+				if(!e.target.closest('#mob-sort-open')){
+					sortClose();
+				}
+			}
+
+			sortToggle = function(e) {
+				// e.stopPropagation();
+				if(e.target.tagName !== 'SPAN'){
+					$(e.target).closest('.mob-sort__button').toggleClass('active');
+				}
+			}
+
+			sortClose = function() {
+				$sortToggler.removeClass('active');
+			}
+
+			init();
+		})();
+	}
+	// END mobile catalog filter handler
 	
 	function init(){     
        var myMap = new ymaps.Map("ymapsID_263", {
